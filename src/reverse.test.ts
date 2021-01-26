e60a4fe995e8398ce69f8bb366b1ed3f986134c9
Chris Chu <op.github.io@gmail.com>
2021-01-26 23:11:40 +0800

    Add more edge cases; unify return rule for invalid inputs :mag:

--- src/reverse.test.ts
+++ src/reverse.test.ts
@@ -6,7 +6,7 @@ describe('可自訂要選用哪個 child node，有簡單容錯機制', (): void
 
   const getLargest = (entries) => entries.reduce((a, b) => b[0] > a[0] ? b : a);
 
-  const largeTree = {
+  const complex = {
     獎: {
       大: { 要: '我' },
       頭: {
@@ -16,13 +16,35 @@ describe('可自訂要選用哪個 child node，有簡單容錯機制', (): void
     },
   };
 
+  const defaultResultOfComplexTree = { 我: { 要: { 大: '獎' } } };
+  const customResultOfComplexTree = { 尾: { 牙: { 中: { 頭: '獎' } } } };
+
+  const multiple = {
+    boolean: true,
+    null: null,
+    number: 2,
+    string: '1',
+  };
+
+  const nth = (n) => (entries) => entries[n];
+
   test.each`
-    params                     | expected                              | comments
-    ${[exampleInput]}          | ${exampleOutput}                      | ${'考題本身'}
-    ${[null]}                  | ${null}                               | ${'容錯: null'}
-    ${[{}]}                    | ${{}}                                 | ${'容錯: empty object'}
-    ${[largeTree]}             | ${{ 我: { 要: { 大: '獎' } } }}         | ${'使用預設 filter'}
-    ${[largeTree, getLargest]} | ${{ 尾: { 牙: { 中: { 頭: '獎' } } } }} | ${'套用自定義 node filter'}
+    params                                   | expected                      | comments
+    ${[exampleInput]}                        | ${exampleOutput}              | ${'基本測試'}
+    ${[null]}                                | ${null}                       | ${'無法處理的 input，輸入值非 Object instance'}
+    ${[{}]}                                  | ${null}                       | ${'無法處理的 input，底層為空物件'}
+    ${[{ one: {} }]}                         | ${null}                       | ${'無法處理的 input，一層為空物件'}
+    ${[{ two: { one: {} } }]}                | ${{ one: 'two' }}             | ${'部分可處理的 input，二層後出現空物件'}
+    ${[complex]}                             | ${defaultResultOfComplexTree} | ${'使用預設 filter'}
+    ${[complex, getLargest]}                 | ${customResultOfComplexTree}  | ${'套用自定義 node filter'}
+    ${[multiple]}                            | ${{ true: 'boolean' }}        | ${'使用預設 filter，處理特殊 value'}
+    ${[multiple, nth(0)]}                    | ${{ true: 'boolean' }}        | ${'自定義 filter，value 為 boolean'}
+    ${[multiple, nth(1)]}                    | ${{ null: 'null' }}           | ${'自定義 filter，value 為 null'}
+    ${[multiple, nth(2)]}                    | ${{ 2: 'number' }}            | ${'自定義 filter，value 為 number'}
+    ${[multiple, nth(3)]}                    | ${{ 1: 'string' }}            | ${'自定義 filter，value 為 string'}
+    ${[multiple, () => null]}                | ${null}                       | ${'自定義 filter，底層回 null'}
+    ${[{ multiple }, () => null]}            | ${null}                       | ${'自定義 filter，一層回 null'}
+    ${[{ nodes: { multiple } }, () => null]} | ${{ multiple: 'nodes' }}      | ${'自定義 filter，二層後回 null，應可以顯示部分結果'}
   `(
     '$comments',
     ({ params, expected }) => expect(reverse(...params)).toEqual(expected),
--- src/reverse.ts
+++ src/reverse.ts
@@ -21,7 +21,7 @@ function reverseNodes(node: Path|string, filter: EntriesFilter, exists: Path|str
   switch (entries.length) {
     // Empty object, 如果是 root, 回傳 {}; 如果是內層, 略過此層
     case 0:
-      return exists instanceof Object ? exists : {};
+      return exists instanceof Object ? exists : null;
     // 標準狀況, 單一 key-value
     case 1:
       [[key, child]] = entries;
@@ -29,7 +29,10 @@ function reverseNodes(node: Path|string, filter: EntriesFilter, exists: Path|str
     // 多 key-value 時，透過 filter 決定要使用的 entry
     default:
       entry = filter(<Entries> entries);
-      if (entry === null) return exists instanceof Object ? exists : null;
+      if (entry === null) {
+        // filter 回 null，代表 sub-nodes 都不要，應該要停在 exists 部分
+        return exists instanceof Object ? exists : null;
+      }
       [key, child] = entry;
       break;
   }
@@ -46,6 +49,7 @@ function defaultEntriesFilter(entries: Entries): Entry|null {
 }
 
 function reverse(nodes: Path|null, filter: EntriesFilter = defaultEntriesFilter) {
+  // 不合規格的 inputs，回 null
   if (!(nodes instanceof Object)) return null;
 
   return reverseNodes(nodes, filter);
